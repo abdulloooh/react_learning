807c2a303ac15711e393a13e48743c06f398e855
abdulloooh <abdullahakinwumi@gmail.com>
2020-04-11 18:53:57 +0100

    removed main file from this vidly branch

--- first_react/my_src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const element = <p>My first react app is awesome</p>;
-console.log(element);
-
-ReactDOM.render(element, document.getElementById("root"));
