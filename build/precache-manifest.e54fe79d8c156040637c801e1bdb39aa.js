cd9d25db4195d2dc0f0e08d16d9b22d9b5b34921
abdulloooh <abdullahakinwumi@gmail.com>
2020-05-27 16:04:56 +0100

    brough app back to root

---
+++ build/precache-manifest.e54fe79d8c156040637c801e1bdb39aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e658e81acec552af1866e4c5ddf6f02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ae7e3421fcf48c1265",
+    "url": "/static/css/2.889f81a2.chunk.css"
+  },
+  {
+    "revision": "60f0f27e47501d8224e2",
+    "url": "/static/css/main.7f1eae80.chunk.css"
+  },
+  {
+    "revision": "f2ae7e3421fcf48c1265",
+    "url": "/static/js/2.74a2167e.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/static/js/2.74a2167e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60f0f27e47501d8224e2",
+    "url": "/static/js/main.0e8701e1.chunk.js"
+  },
+  {
+    "revision": "eaa42bd0596bc57c3289",
+    "url": "/static/js/runtime-main.924d2968.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
